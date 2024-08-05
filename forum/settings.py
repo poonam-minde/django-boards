f81c2a6538888cf25e92d785b23cf5ac1bca7616
poonam-minde <poonam@testpress.in>
2024-08-05 08:55:56 +0000

    SQLite database

--- forum/settings.py
+++ forum/settings.py
@@ -81,9 +81,10 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
