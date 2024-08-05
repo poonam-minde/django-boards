d856184f888407b8438b221d1ca64bd1dcd0f112
poonam-minde <poonam@testpress.in>
2024-08-05 06:33:52 +0000

    Add requirements.txt file

--- forum/settings.py
+++ forum/settings.py
@@ -19,14 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+from decouple import config, Csv
+import dj_database_url
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-98rt*g=%zr4qnbdg5vnik4mazyul-_vqtw15-(-wkkvczj$k&a'
+SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-98rt*g=%zr4qnbdg5vnik4mazyul-_vqtw15-(-wkkvczj$k&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -78,10 +81,9 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
